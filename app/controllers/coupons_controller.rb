360535411de3156e6a2dc80ffa588cc1e7de3248
sophie <justanothersophie@gmail.com>
2021-09-07 23:58:23 -0400

    New view, new and create controller actions

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,6 +4,8 @@ class CouponsController < ApplicationController
     end
 
     def create
+        @new_coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
+        redirect_to coupon_path(@new_coupon)
     end
 
     def show
